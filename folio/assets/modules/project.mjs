0005e94a80a62d0b7bc77b6b75ea60196f32553d
Eric Laursen <ericjlaursen@gmail.com>
2020-03-31 19:50:03 -0400

    more work on shadowDom + component

--- folio/assets/modules/project.mjs
+++ folio/assets/modules/project.mjs
@@ -1,6 +1,4 @@
 (function (){
-	
-	console.log('TEST');
 	//create element
 	let templateStr = document.createElement('template');
 
@@ -20,7 +18,7 @@
 		href="./v2-hover.css">
 	<div class="col-3-4 hover-v2">
      <a target="_blank" class="box mastering-bg" href="https://www.ntuclearninghub.com/udemy/course/?id=2421558">
-        <div class="caption"">
+        <div class="caption">
            <p>HEADER</p>
            <hr />
            <p class="sub-text">An online course I created covering D3v5, interactive mapping, linked visualizations, 3rd party api integration & more.</p>
@@ -34,12 +32,9 @@
      </a>
   </div>`
 
-  //hmm!
   class Project extends HTMLElement{
   	constructor(){
 			super()
-			console.log('Constructor');
-
 			let shadowRoot = this.attachShadow({
 				mode: 'open'
 			})
@@ -48,19 +43,87 @@
 			shadowRoot.appendChild(templateStr.content.cloneNode(true))
   	}
 
+  	/*
+			for EVERY attr 
+			a get attr() && set attr() are necessary
+  	*/
+  	get heading(){
+  		return this.hasAttribute('heading')
+  	}
+
+  	set heading(val){
+  		console.log('----set heading(val)----');
+  		console.log(val)
+  		
+  		return val ? 
+  			this.setAttribute(val.toString(), val) : 
+  			this.removeAttribute(val.toString())
+  	}
+
+  	static get observedAttribute(){
+  		return ['heading']
+  	}
+
+  	_setModuleAttributes(){
+  		//loop through attrs
+  		for( let i=0; i< this.attributes.length; i++) { 
+
+  			let thisAttr = this.attributes[i]
+  			console.log('thisAttr')
+  			console.log(thisAttr)
+  			console.log('%c // - - - - - //', 'background-color: yellow; color: black;')
+  			
+  			//if attr is present && has a name
+  			if( !this.hasAttribute(thisAttr.name)) { 
+  				/*
+						//set the attr name on the attributes obj
+						this.attributes[newAttrName]
+  				*/
+  				this.setAttribute(thisAttr.name, this.getAttribute[i]||true); 
+  			} 
+  		}
+  	}
+
+  	//shadow-dom method
   	connectedCallback(){
+  		this._setModuleAttributes();
+
 	  	window.customElements.whenDefined('portfolio-project')
 	  	.then(() => {
-	  		let attrVal = this.attributes[0].value
-	  		templateStr.innerHTML = templateStr.innerHTML.replace('HEADER', attrVal)
+	  		//SETT attr prop
+	  		templateStr.innerHTML = templateStr.innerHTML.replace('HEADER', 'tomato') //this.attributes?.heading
+	  		console.log('templateStr')
+	  		console.log(templateStr)
+				window.requestAnimationFrame(() => {console.log('!')});	  		
 	  	})
   	}
+
+  	//shadow-dom method
+  	disconnectedCallback(){
+
+  	}
+
+  	//custom method
+  	_handleNestedComponentsLoaded(){
+
+  	}
+
+  	/*
+			htmlElement method
+			ONLY handle side-effects that interact with the dom
+  	*/
+  	attributeChangedCallback(attrName,oldVal,newVal){
+
+  	}  	
 
   //class-ending
   }
 
-  //specific naming convention word-word
-  //passing the above class 'Project'
+  /*
+  	specific naming convention word-word
+  	passing the above class 'Project'
+  	dispatches / allows api access to callback events (connected && disconnected, above)
+  */
   window.customElements.define('portfolio-project', Project)
 
 })()
